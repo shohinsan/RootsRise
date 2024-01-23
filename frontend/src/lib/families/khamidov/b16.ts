e703685e412509acea8c0e05352db07790703f48
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-01-22 20:14:19 -0800

    chore: add new member

---
+++ frontend/src/lib/families/khamidov/b16.ts
@@ -0,0 +1,28 @@
+import type { Base } from '$customTypes';
+
+const b16: Base[] = [
+	{
+		id: 'B16A',
+		name: 'Somon ...',
+		dependant: 'B6B',
+		information: {
+			birth: '1933'
+		},
+		description: {
+			details: ['A short description', 'Another description']
+		}
+	}
+	// {
+	// 	id: 'B16B',
+	// 	name: 'Add text here',
+	// 	dependant: 'B16A',
+	// 	information: {
+	// 		birth: '1933'
+	// 	},
+	// 	description: {
+	// 		details: ['A short description', 'Another description']
+	// 	}
+	// }
+];
+
+export default b16;
