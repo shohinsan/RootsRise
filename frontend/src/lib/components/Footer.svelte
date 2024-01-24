897fbfd3f0449b4078cde28c8379a47dd56ef591
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-01-23 16:53:21 -0800

    chore: add Footer component

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<footer>
+	<p>
+        A large database of family members and their relations.
+    </p>
+
+	<a
+		class="issues"
+		href="https://github.com/shohinsan/RootsRise/issues/new?assignees=&labels=issue&projects=&template=issue-temp.yaml"
+		>Report an issue 🚀</a
+	>
+	<a
+		class="issues"
+		href="https://github.com/shohinsan/RootsRise/issues/new?assignees=&labels=feedback&projects=&template=feedback-temp.yaml"
+		>Your feedback is appreciated 🌟</a
+	>
+</footer>
+
+<style>
+	footer {
+		border-top: 1px solid var(--color-text);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 1rem;
+		font-size: 1em;
+		color: var(--color-text);
+	}
+</style>
